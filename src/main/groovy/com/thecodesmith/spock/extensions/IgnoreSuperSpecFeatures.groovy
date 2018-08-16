2d3fa3acdb12140faafd45afe3b76662705194a0
Brian Stewart <brian@thecodesmith.com>
2018-08-16 11:13:04 -0500

    Add JavaDoc

--- src/main/groovy/com/thecodesmith/spock/extensions/IgnoreSuperSpecFeatures.groovy
+++ src/main/groovy/com/thecodesmith/spock/extensions/IgnoreSuperSpecFeatures.groovy
@@ -7,6 +7,17 @@ import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 
+/**
+ * Indicates that the specified feature methods from the superclass
+ * (which inherits from {@code spock.lang.Specification}) should not be run.
+ * <p>
+ * To override a specific feature method (rather than ignoring it),
+ * use {@link OverrideSuperSpec}.
+ *
+ * @see OverrideSuperSpec
+ *
+ * @author Brian Stewart
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @ExtensionAnnotation(IgnoreSuperSpecFeaturesExtension)
--- src/main/groovy/com/thecodesmith/spock/extensions/OverrideSuperSpec.groovy
+++ src/main/groovy/com/thecodesmith/spock/extensions/OverrideSuperSpec.groovy
@@ -7,6 +7,19 @@ import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 
+/**
+ * Indicates that the target feature method overrides a method from the superclass
+ * (which inherits from {@code spock.lang.Specification}). Spock's default behavior
+ * is to run both the superclass feature method and subclass feature method.
+ * This annotation causes the overridden superclass feature method to not be run.
+ * <p>
+ * To ignore superclass feature methods (rather than overriding them),
+ * use {@link IgnoreSuperSpecFeatures}.
+ *
+ * @see IgnoreSuperSpecFeatures
+ *
+ * @author Brian Stewart
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @ExtensionAnnotation(OverrideSuperSpecExtension)
